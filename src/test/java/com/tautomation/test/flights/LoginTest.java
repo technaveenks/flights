0bb6a813d27a3e099d3a17387150e65c77c1d25a
TechFundas <tech.naveenks@gmail.com>
2018-09-23 12:31:15 +0530

    added method to capture screeshot

--- src/test/java/com/tautomation/test/flights/LoginTest.java
+++ src/test/java/com/tautomation/test/flights/LoginTest.java
@@ -5,8 +5,15 @@ import org.openqa.selenium.WebDriver;
 public class LoginTest {
 	WebDriver driver;
 
+	public void captureScreenshot(){
+		
+	}
+	
 	public static void main(String args[]){
 		
 		System.out.println("Naviagted");
 	}
+	
+
+	
 }
